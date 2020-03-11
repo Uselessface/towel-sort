fb368b7f39d64b85921cd40524a45e17bbfcc078
Uselessface <60576572+Uselessface@users.noreply.github.com>
2020-03-12 00:07:28 +0400

    added solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
+// пройтись по массиву for
+// для каждого нечетного элемента определяем при помощи $
+// из всего этого либо генерируем нвоый массив, либо в этот же массив
+//пройтись по итоговому массиву и сделать джоин в строку и вернуть строку
 
-// You should implement your task here.
+//Math библиотека для array problems max min и avg элементы
+module.exports = function towelSort (matrix=[]) {
 
-module.exports = function towelSort (matrix) {
-  return [];
+let a=[];
+for (let i = 0; i < matrix.length; i++) {
+	if (i % 2===1) {
+		a.push(matrix[i].reverse());
+	}
+	else {
+		a.push(matrix[i]);
+	}
+
+}
+let s=[];
+for (let i = 0; i < a.length; i++) {
+	s=s.concat(a[i]);
+}
+  return s;
 }
